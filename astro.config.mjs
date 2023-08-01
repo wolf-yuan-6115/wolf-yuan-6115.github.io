c2bee412fc8c20c06b1084f772fd0e79c0d396b0
wolf-yuan-6115 <h1030035@gmail.com>
2023-08-01 16:43:55 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind()],
+  adapter: vercel({
+    analytics: true,
+  }),
 });
