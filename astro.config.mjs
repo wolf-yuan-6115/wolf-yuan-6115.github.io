8d7e696028bd530f58fb8b5ee2a7509e3e626ea3
wolf-yuan-6115 <h1030035@gmail.com>
2023-08-01 16:48:35 +0800

    Update Vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/edge";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
   adapter: vercel({
     analytics: true,
   }),
+  output: "server",
 });
